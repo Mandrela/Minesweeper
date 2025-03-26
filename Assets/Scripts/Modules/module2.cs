00b6c28b359fc41372d0b13563133b0adc6c02f5
Digital-Bogila <Timonbogdanov31@mail.ru>
2025-03-26 22:02:20 +0300

    Light_Module_almost_it's_so_close
    asf

--- Assets/Scripts/Modules/module2.cs
+++ Assets/Scripts/Modules/module2.cs
@@ -12,6 +12,10 @@ public class module2 : MonoBehaviour
     [SerializeField] StringSet Guidelines;
     public string moduleName = "lights"; 
     string guideline;
+    private int needed_color;
+    private int very_wrong_color;
+    public Animator animatorik_LIGHT;
+    public Animator animatorik_BUTTON;
 
     [SerializeField] ModuleLineTemplates lineTemplates; 
     string replaceMark = "%mark%";
@@ -22,17 +26,39 @@ public class module2 : MonoBehaviour
     Color NormalColor;
     Renderer rend;
 
+    bool fl = true;
+
     void Start()
     {
+        needed_color = Utils.Next(3);
+        very_wrong_color = Utils.Next(3);
+        while (very_wrong_color == needed_color)
+        {
+            very_wrong_color = Utils.Next(3);
+        }
         this.rend = GetComponent<Renderer>();
         this.HighlightColor.a = this.Alpha;
         this.NormalColor = this.rend.material.color;
+        StartCoroutine(Timer());
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdateWAIT()
     {
-        
+        animatorik_LIGHT.SetInteger("Color_ID", Utils.Next(3));
+    }
+
+    private void Update()
+    {
+        if(animatorik_BUTTON.GetBool("IsPressed")&&(animatorik_LIGHT.GetInteger("Color_ID") == needed_color))
+        {
+            ModuleSolvedEvent.Raise();
+            fl = false;
+            Debug.Log("Lights were defused");
+        }
+        else if(animatorik_BUTTON.GetBool("IsPressed") && (animatorik_LIGHT.GetInteger("Color_ID") != needed_color))
+        {
+            ModuleLostEvent.Raise();
+        }
     }
 
     void OnMouseEnter()
@@ -42,5 +68,14 @@ public class module2 : MonoBehaviour
     void OnMouseExit()
     {
         this.rend.material.color = this.NormalColor;
+    }
+
+    IEnumerator Timer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.3f);
+            FixedUpdateWAIT();
+        }
     }
 }
