d66b23afbd4804a46ff566f2ea7e57f9f2593bc2
Digital-Bogila <Timonbogdanov31@mail.ru>
2025-03-26 19:28:53 +0300

    Light_Begin
    asf

---
+++ Assets/Scripts/Modules/module2.button.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Light_button : MonoBehaviour
+{
+    public Color HighlightColor;
+    public float Alpha = 1f;
+    public Animator animatorius;
+    
+    
+    
+    Color NormalColor;
+    Renderer rend;
+    module2 ParentScript;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        HighlightColor.a = Alpha;
+        NormalColor = rend.material.color;
+        ParentScript = GetComponentInParent<module2>();
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = HighlightColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = NormalColor;
+        animatorius.SetBool("IsPressed", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        animatorius.SetBool("IsPressed", true);
+    }
+
+    private void OnMouseUp()
+    {
+        animatorius.SetBool("IsPressed", false);
+    }
+}
